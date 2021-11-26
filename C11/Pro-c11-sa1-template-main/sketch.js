9cdbb626abcf1c3cd391eab67d655381f6bbd64b
Prakashrajpurohit4 <94972776+Prakashrajpurohit4@users.noreply.github.com>
2021-11-26 18:57:52 +0530

    Add files via upload

---
+++ C11/Pro-c11-sa1-template-main/sketch.js
@@ -0,0 +1,29 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var s1,s2,s3,s4
+var sp=[]
+function setup() {
+  createCanvas(400,400);
+s1=createSprite(100,100,30,30)
+
+s2=createSprite(200,100,30,30)
+
+s3=createSprite(300,100,30,30)
+
+s4=createSprite(400,100,30,30)
+sp=[s1,s2,s3,s4];
+for(var i=0;i<sp.length;i++){
+console.log(sp[i].position.x)
+
+
+
+}
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
